aec64282fe0f09aa18d56d003957371d721f4f23
Renzo Damian <renzo.damian.go@gmail.com>
2022-02-07 22:31:13 -0500

    improve clickable area in navbar buttons

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,31 +9,31 @@ export const NavBar = () => {
                     {/* <span id={styles.block}>█</span> */}
                 </h1>
                 <ul className={styles.nav_links}>
-                    <li className={styles.flex_child}>
                         <Link href={'/'}>
                             <a>
+                    <li className={styles.flex_child}>
                             Projectos
 
+                    </li>
                             </a>
 
                         </Link>
 
-                    </li>
-                    <li className={styles.flex_child}>
                         <Link href={'/acerca-de-mi'}>
                             <a>
+                    <li className={styles.flex_child}>
                                 Acerca de mí
+                    </li>
                             </a>
                         </Link>
 
-                    </li>
-                    <li className={styles.flex_child}>
                         <a href="mailto:renzo.damian.go@gmail.com">
+                    <li className={styles.flex_child}>
 
                         Contáctame
-                        </a>
 
                     </li>
+                        </a>
                 </ul>
 
 
