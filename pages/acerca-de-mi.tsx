81938105dc288ab527d4dc8468d8c258e5415fee
Renzo Damian <renzo.damian.go@gmail.com>
2022-02-08 00:50:31 -0500

    add cover img to proyectos page

--- pages/acerca-de-mi.tsx
+++ pages/acerca-de-mi.tsx
@@ -1,8 +1,12 @@
+import Layout from "../components/layout";
 import { NavBar } from "../components/navbar";
 
 export default function About() {
     return (
         <>
+            <Layout>
+                
+            </Layout>
 
 
         </>
--- pages/proyectos/camupc.tsx
+++ pages/proyectos/camupc.tsx
@@ -1,7 +1,28 @@
+import Layout from '../../components/layout'
+import styles from '../../styles/proyecto.module.scss'
+
 export default function camupc() {
     return (
-        <>
+        <div className={styles.proyecto}>
+            <Layout>
 
-        </>
+                <div className={styles.bg_img}>
+                    <img src="../../img/banner.png" alt="" />
+
+                </div>
+                <div>
+
+                </div>
+                <div className={styles.container} id={styles.content}>
+                    <ul>
+                        <li><span className='bold'>Proyecto:</span> Congreso Anual de Marketing UPC</li>
+                        <li><span className='bold'>Rol:</span> Estudiante </li>
+                        <li><span className='bold'>Duración:</span> Julio - Sept 2021</li>
+                    </ul>
+
+                </div>
+            </Layout>
+
+        </div>
     )
 }
